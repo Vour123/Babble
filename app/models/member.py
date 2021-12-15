c359a0db267adb2f9468dea8ac21d8c38fc6c177
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-12-15 17:30:40 -0600

    made to_dict function to return data

--- app/models/member.py
+++ app/models/member.py
@@ -1,4 +1,7 @@
 from .db import db
+from .user import User
+from .server import Server
+
 
 class Member(db.Model):
     __tablename__ = 'members'
@@ -6,6 +9,3 @@ class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, foreignKey=('user.id'), nullable=False)
     server_id = db.Column(db.Integer, foreignKey=('server.id'), nullable=False)
-
-    user = db.relationship('User', back_populates='members')
-    server = db.relationship('Server', back_populates='members')
--- app/models/server.py
+++ app/models/server.py
@@ -1,4 +1,5 @@
 from .db import db
+import datetime
 
 class Server(db.Model):
     __tablename__ = 'servers'
@@ -12,3 +13,18 @@ class Server(db.Model):
     users = db.relationship('User', back_populates='servers')
     members = db.relationship('User', secondary='members', back_populates='servers')
     channels = db.relationship('Channel', back_populates='servers', cascade='all, delete')
+
+    def to_dict(self):
+        if self.private and len(self.channels):
+            channel = self.channels[0].to_dict()
+            channelId = channel["id"]
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image_url': self.image_url,
+            'private': self.private,
+            'owner_id': self.owner_id,
+            'members': [user.id for user in self.members],
+            "member_list": [user.to_dict() for user in self.members],
+            'channels': {channelId: self.channels[0].to_dict()} if len(self.channels) and self.private else{}
+        }
