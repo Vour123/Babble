bec34937cfae3a0ae1cbd816f69803aaad7fc048
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-12-15 17:37:21 -0600

    bug fixes

--- app/models/member.py
+++ app/models/member.py
@@ -7,5 +7,5 @@ class Member(db.Model):
     __tablename__ = 'members'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, foreignKey=('user.id'), nullable=False)
-    server_id = db.Column(db.Integer, foreignKey=('server.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    server_id = db.Column(db.Integer, db.ForeignKey('server.id'), nullable=False)
