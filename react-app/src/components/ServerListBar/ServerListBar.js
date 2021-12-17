e4be9729dcbd3e66ed710b57f831ef5e3474dca8
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-12-17 16:07:12 -0600

    showing server image on frontend route /servers

--- react-app/src/components/ServerListBar/ServerListBar.js
+++ react-app/src/components/ServerListBar/ServerListBar.js
@@ -4,18 +4,21 @@ import { getAllServers } from '../../store/server'
 
 
 export default function ServerListBar() {
-    const allServers = useSelector(state => state.server)
+    const allServers = useSelector(state => Object.values(state.server))
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getAllServers())
     },[dispatch])
 
-    console.log('this is allServer', allServers)
-
+    console.log('this is all servers', allServers)
     return (
-        <div>
-            
+        <div className='all-servers-container'>
+            {allServers?.map((singleServer) => {
+                return (
+                    <img src={singleServer.image_url}></img>
+                )
+            })}
         </div>
     )
 }
