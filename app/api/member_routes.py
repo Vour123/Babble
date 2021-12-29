a809a36d8e9461845b30dc7b4bcaccb31c40e7ed
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-12-28 20:32:02 -0600

    fixed minor bugs in member routes, also fixed websockets

--- app/api/member_routes.py
+++ app/api/member_routes.py
@@ -3,6 +3,7 @@ from flask_login.utils import login_required
 from flask_login import login_required, current_user
 from app.models import db, Server, User, server
 from app.models.member import Member
+from app.socket import handle_edit_a_server
 
 member_routes = Blueprint('members', __name__)
 
@@ -18,30 +19,31 @@ def add_new_member_to_server(server_id, member_id):
         )
         db.session.add(new_member)
         db.session.commit()
-        #web socket to add member to server, i think this would be edit server.
-        return server.to_dict()
+        handle_edit_a_server(existing_server.to_dict())
+        return existing_server.to_dict()
     return {"errors": "Cannot add user to server"}
 
-@member_routes.route('/<int:server_id>/members/<int:member_id>', methods=['DELETE'])
+@member_routes.route('/<int:server_id>/members', methods=['DELETE'])
 @login_required
 def delete_existing_member_from_server(server_id):
     existing_user = User.query.get(int(current_user.id))
     existing_server = User.query.get(int(server_id))
-    if existing_user in existing_server:
+    if existing_user in existing_server.members:
         # find the user in the members list.
         member = Member.query.filter(current_user.id == Member.user_id)
         member.filter(existing_server == Member.server_id)[0]
-        db.session.delete()
+        db.session.delete(member)
         db.session.commit()
+        handle_edit_a_server(existing_server.to_dict())
         return {"status": "success"}
     return {"stauts": "failure"}
 
-@member_routes.route('/<int:server_id>/members', methods=['PUT'])
+@member_routes.route('/<int:server_id>/members', methods=['POST'])
 @login_required
 def edit_members_on_existing_server(server_idP):
     user = User.query.get(int(current_user.id))
     server = Server.query.get(int(server_idP))
-    if user and (user not in server):
+    if user and (user not in server.members):
         member = Member(
             user_id = current_user.id,
             server_id = server_idP
