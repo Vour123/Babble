816f4b28ff25af587348d6a7fadf6935d49147b5
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-12-31 06:08:08 -0600

    made a splash page, not done yet. posting messages to my state.

--- react-app/src/components/MessagesBox/MessagesBox.js
+++ react-app/src/components/MessagesBox/MessagesBox.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react'
+import { postMessageToServer } from '../../store/server';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import DisplayMessages from '../DisplayMessages/DisplayMessages'
 import './MessagesBox.css'
 
 export default function MessagesBox({channelName}) {
     const [message, setMessage] = useState('');
+    const sessionUserId = useSelector(state => state.user?.id)
+    const { specificServerId, specificChannelId } = useParams() 
+    const dispatch  = useDispatch();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const messageInformation = {
+            content: message,
+            specificChannelId: +specificChannelId,
+            sessionUserId: +sessionUserId
+        }
+        if(messageInformation) {
+            await dispatch(postMessageToServer(messageInformation, +specificServerId)).then(setMessage(''));
+        }
+    }
 
     return (
         <>
@@ -19,7 +37,9 @@ export default function MessagesBox({channelName}) {
                     value={message}
                     className='new-message-input message-input'
                     onChange={(e) => setMessage(e.target.value)}
+                    onSubmit={handleSubmit}
                     />
+                    <button onClick={handleSubmit}>click</button>
                 </div>
         </>
     )
--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './SplashPage.css'
+import nomad from './assets/nomad1.svg'
 
 export default function SplashPage() {
 
     return (
-        <div>
-            splash page
+        <div className='splash-page-container'>
+            <div className='welcome-message'>Welcome to Babble!</div>
+            <div className='description'>
+                Babble is a real-time chat application. 
+            </div>
+            <img className='splash-png' src={nomad}></img>
         </div>
     )
 }
